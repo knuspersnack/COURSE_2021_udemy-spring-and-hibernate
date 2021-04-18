1daa6ae415a5d90803a14b0667822d50f5597334
knuspersnack <thumos@posteo.gr>
2021-04-18 13:36:48 +0200

    Revert "Finished"
    This reverts commit 3d14586e14b0bf029144429da92ed5c3b097927f.

--- src/main/java/com/udemy/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/udemy/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,6 @@
 package com.udemy.aopdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
@@ -11,14 +10,6 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
-
-    @After("execution(* com.udemy.aopdemo.dao.AccountDAO.findAccounts(..))")
-    public void afterFinallyFindAccountsAdvice(JoinPoint joinPoint){
-        String method = joinPoint.getSignature().toShortString();
-        System.out.println("--> ADVICE: Executing @After advice on method: " + method);
-
-        System.out.println("<--");
-    }
 
     @AfterThrowing(pointcut = "execution(* com.udemy.aopdemo.dao.AccountDAO.findAccounts(..))", throwing = "exc")
     public void afterThrowingFindAccountsAdvice(JoinPoint joinPoint, Throwable exc) {
