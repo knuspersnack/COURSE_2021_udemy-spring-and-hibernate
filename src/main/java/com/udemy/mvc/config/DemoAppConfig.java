611798d3f01be0b3fcca6b98cd10f45890a37837
knuspersnack <thumos@posteo.gr>
2021-04-20 14:40:55 +0200

    Retrieving users and passwords from database

--- src/main/java/com/udemy/mvc/config/DemoAppConfig.java
+++ src/main/java/com/udemy/mvc/config/DemoAppConfig.java
@@ -1,16 +1,30 @@
 package com.udemy.mvc.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.udemy.mvc")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+    @Autowired
+    private Environment env; // will hold data read from the property file
+
+    private Logger logger = Logger.getLogger(getClass().getName());
 
     // Define a bean for ViewResolver
     @Bean
@@ -22,6 +36,49 @@ public class DemoAppConfig {
 
         return viewResolver;
     }
+
+    // define a bean for our security datasource
+    @Bean
+    public DataSource securityDataSource() {
+        //create connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        //set te jdbc driver class
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+
+        String jdbcUrl = env.getProperty("jdbc.url");
+        String jdbcUser = env.getProperty("jdbc.user");
+        String jdbcPassword = env.getProperty("jdbc.password");
+
+        //log the connection props
+        logger.info("--> jdbc.url=" + jdbcUrl);
+        logger.info("--> jdbc.user=" + jdbcUser);
+
+        //set database connection props
+        securityDataSource.setJdbcUrl(jdbcUrl);
+        securityDataSource.setUser(jdbcUser);
+        securityDataSource.setPassword(jdbcPassword);
+
+        //set connection pool props
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    //Helper method
+    //read environment property and convert to int
+    private int getIntProperty(String propName) {
+        String propVal = env.getProperty(propName);
+        int intPropVal = Integer.parseInt(propVal);
+        return intPropVal;
+    };
 
 
 
