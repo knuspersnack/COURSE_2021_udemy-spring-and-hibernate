7de95140c9fdae752e2c7bf465e8ec9945b75ce5
knuspersnack <thumos@posteo.gr>
2021-04-21 20:04:17 +0200

    The exceptions are now catched not only for a single rest controller, but globally

---
+++ src/main/java/com/udemy/mvc/rest/RestExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.udemy.mvc.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+//Global exception handler
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    //This exception handler will catch StudentNotFoundExceptions
+    @ExceptionHandler
+    public ResponseEntity<CustomErrorResponse> handleException(StudentNotFoundException exc) {
+
+        CustomErrorResponse error = new CustomErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<CustomErrorResponse> handleException(Exception exc) {
+
+        CustomErrorResponse error = new CustomErrorResponse();
+
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
+}
--- src/main/java/com/udemy/mvc/rest/StudentController.java
+++ src/main/java/com/udemy/mvc/rest/StudentController.java
@@ -51,31 +51,6 @@ public class StudentController {
 		return theStudents.get(studentId);
 		
 	}
-
-	//This exception handler will catch StudentNotFoundExceptions
-	@ExceptionHandler
-	public ResponseEntity<CustomErrorResponse> handleException(StudentNotFoundException exc) {
-		
-		CustomErrorResponse error = new CustomErrorResponse();
-		
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-	 }
-
-	@ExceptionHandler
-	public ResponseEntity<CustomErrorResponse> handleException(Exception exc) {
-		
-		CustomErrorResponse error = new CustomErrorResponse();
-		
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setMessage(exc.getMessage());
-		error.setTimeStamp(System.currentTimeMillis());
-		
-		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-	}	
 }
 
 
