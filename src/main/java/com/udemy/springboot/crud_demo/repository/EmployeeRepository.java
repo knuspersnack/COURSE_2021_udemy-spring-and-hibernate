486d9044990754ee8bcc9e0ff4b8b52a8b65fed4
knuspersnack <thumos@posteo.gr>
2021-04-24 18:57:46 +0200

    Trying other features

--- src/main/java/com/udemy/springboot/crud_demo/repository/EmployeeRepository.java
+++ src/main/java/com/udemy/springboot/crud_demo/repository/EmployeeRepository.java
@@ -3,7 +3,10 @@ package com.udemy.springboot.crud_demo.repository;
 
 import com.udemy.springboot.crud_demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
