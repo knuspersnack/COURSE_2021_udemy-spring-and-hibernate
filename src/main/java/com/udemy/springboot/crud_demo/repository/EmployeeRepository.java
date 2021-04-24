1f4d77f6f930d839899b48b9740d4267390b1951
knuspersnack <thumos@posteo.gr>
2021-04-24 21:13:03 +0200

    Simple app

--- src/main/java/com/udemy/springboot/crud_demo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.udemy.springboot.crud_demo.repository;
-
-
-import com.udemy.springboot.crud_demo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
--- src/test/java/com/udemy/springboot/demo/ThymeleafDemoApplicationTests.java
+++ src/test/java/com/udemy/springboot/demo/ThymeleafDemoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CrudDemoApplicationTests {
+class ThymeleafDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
